8c89fc684d345f931c7dcf16f6e3b98af4c03e4e
hwhinowa <jejinoes1@gmail.com>
2018-12-03 00:05:28 +0900

    'router'

--- src/Root.js
+++ src/Root.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+// import { BrowserRouter as Router, Route } from 'react-router-dom';
 import store from './store';
 
 import App from 'components/App';
--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+// import { Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Home, PlanetDetail } from 'pages';
 
 class App extends Component {
     render(){
         return(
-            <div>
-                <Home/>
-                {/* <Route exact path='/' component={Home}/> */}
-                {/* <Route path='/detail' component={PlanetDetail}/> */}
-            </div>
+            <Router>
+                <div>
+                    {/* <Home/> */}
+                    <Route exact path='/' component={Home}/>
+                    <Route path='/detail' component={PlanetDetail}/>
+                </div>
+            </Router>
         )
     }
 }
