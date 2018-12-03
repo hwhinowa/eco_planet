fda5469bed78ed3787dbec0b657bc57f2f990e8d
hwhinowa <jejinoes1@gmail.com>
2018-12-03 21:43:18 +0900

    Updates

--- precache-manifest.c76e307a89486168b6cef001fff178bf.js
+++ precache-manifest.c76e307a89486168b6cef001fff178bf.js
@@ -16,19 +16,19 @@ self.__precacheManifest = [
     "url": "/eco_planet/static/js/runtime~main.31f87634.js"
   },
   {
-    "revision": "f64d3cdb00d59cc7d5de",
-    "url": "/eco_planet/static/js/main.f64d3cdb.chunk.js"
+    "revision": "ac300cf37558c62d0736",
+    "url": "/eco_planet/static/js/main.ac300cf3.chunk.js"
   },
   {
     "revision": "6f7815fe43b98636efa6",
     "url": "/eco_planet/static/js/1.6f7815fe.chunk.js"
   },
   {
-    "revision": "f64d3cdb00d59cc7d5de",
+    "revision": "ac300cf37558c62d0736",
     "url": "/eco_planet/static/css/main.b3fc6105.chunk.css"
   },
   {
-    "revision": "13c0ab96b1c9eba3c9d98bfadc03705a",
+    "revision": "2d7fc8eeb8bb25d3fb7c79f4556c293e",
     "url": "/eco_planet/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/eco_planet/precache-manifest.acb2c4d6cc9d4eb787e25be576c93b49.js"
+  "/eco_planet/precache-manifest.c76e307a89486168b6cef001fff178bf.js"
 );
 
 workbox.clientsClaim();
