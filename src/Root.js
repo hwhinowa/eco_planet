25b519098f9482f20ab698a45870717e4445ab76
hwhinowa <jejinoes1@gmail.com>
2018-12-03 21:56:24 +0900

    'build_test'

--- src/Root.js
+++ src/Root.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
+// import { Router, Route, browserHistory } from 'react-router-dom';
 import { Home, PlanetDetail } from 'pages';
 import store from './store';
 
@@ -8,19 +9,19 @@ import App from 'components/App';
 
 const Root = () => {
     return (
-        <Router>
-            <Provider store = {store}>
-                {/* <App /> */}
-                {/* <Route exact path='/' component={Home}/> */}
-                <div>
-                    {/* <Home/> */}
-                    
-                    <Route path='/detail' component={PlanetDetail}/>
-
-                    <Route exact path='/' component={Home}/>
-                </div>
-            </Provider>
-        </Router>
+        <Provider store = {store}>
+            <BrowserRouter>
+                <App/>
+            </BrowserRouter>
+        </Provider>
+        // <Provider store = {store}>
+        //     <Router>
+        //         <div>
+        //             <Route exact path='/' component={Home}/>                    
+        //             <Route path='/detail' component={PlanetDetail}/>
+        //         </div>
+        //     </Router>
+        // </Provider>
     );
 };
 
--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
-// import { Route } from 'react-router-dom';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+// import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Home, PlanetDetail } from 'pages';
 
 class App extends Component {
     render(){
         return(
-            // <Router>
-                <div>
-                    {/* <Home/>
-                    <Route exact path='/' component={Home}/>
-                    <Route path='/detail' component={PlanetDetail}/> */}
-                </div>
-            // </Router>
+            <div>
+                {/* <Home/> */}
+                <Route exact path='/' component={Home}/>
+                <Route path='/detail' component={PlanetDetail}/>
+            </div>
         )
     }
 }
