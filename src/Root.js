0a53bfd6894ab390514cc1b5571bac8847a56e1b
hwhinowa <jejinoes1@gmail.com>
2018-12-03 22:16:29 +0900

    'build_test'

--- src/Root.js
+++ src/Root.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 // import { Router, Route, browserHistory } from 'react-router-dom';
 import { Home, PlanetDetail } from 'pages';
 import store from './store';
@@ -8,21 +8,11 @@ import store from './store';
 import App from 'components/App';
 
 const Root = () => {
-    return (
-        <Provider store = {store}>
-            <BrowserRouter>
-                <App/>
-            </BrowserRouter>
-        </Provider>
-        // <Provider store = {store}>
-        //     <Router>
-        //         <div>
-        //             <Route exact path='/' component={Home}/>                    
-        //             <Route path='/detail' component={PlanetDetail}/>
-        //         </div>
-        //     </Router>
-        // </Provider>
-    );
+    <Provider store = {store}>
+        <Router>
+            <App/>
+        </Router>
+    </Provider>
 };
 
 export default Root;
