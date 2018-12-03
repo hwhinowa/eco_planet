26a5008cf5d312652f6bcd6c87a9f895cf598641
hwhinowa <jejinoes1@gmail.com>
2018-12-03 21:32:55 +0900

    'build_test'

--- src/Root.js
+++ src/Root.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import { Provider } from 'react-redux';
+import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Home, PlanetDetail } from 'pages';
 import store from './store';
