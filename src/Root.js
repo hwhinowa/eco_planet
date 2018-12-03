7901355ffd67a2ea607e9e87ac62a72b9887a742
hwhinowa <jejinoes1@gmail.com>
2018-12-03 21:32:09 +0900

    'build_test'

--- src/Root.js
+++ src/Root.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Provider } from 'react-redux';
+// import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Home, PlanetDetail } from 'pages';
 import store from './store';
@@ -8,17 +8,17 @@ import App from 'components/App';
 
 const Root = () => {
     return (
-        <Provider store = {store}>
-            {/* <Router> */}
+        <Router>
+            <Provider store = {store}>
                 {/* <App /> */}
                 {/* <Route exact path='/' component={Home}/> */}
                 <div>
-                    <Home/>
-                    {/* <Route exact path='/' component={Home}/>
-                    <Route path='/detail' component={PlanetDetail}/> */}
+                    {/* <Home/> */}
+                    <Route exact path='/' component={Home}/>
+                    <Route path='/:detail' component={PlanetDetail}/>
                 </div>
-            {/* </Router> */}
-        </Provider>
+            </Provider>
+        </Router>
     );
 };
 
