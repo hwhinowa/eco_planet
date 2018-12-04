826e395d802055248e63d63d5ba869eb280e387a
hwhinowa <jejinoes1@gmail.com>
2018-12-04 23:27:54 +0900

    'test'

--- src/index.js
+++ src/index.js
@@ -11,11 +11,11 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-    <Router>
-        <Provider store = {store}>
+    <Provider store = {store}>
+        <Router>
             <App />
-        </Provider>
-    </Router>,
+        </Router>
+    </Provider>,
     document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
