2b38ceedac3587fb53f9bf13a88d6044a1fea797
hwhinowa <jejinoes1@gmail.com>
2018-12-08 23:57:32 +0900

    'test'

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 // import { Switch, Route, withRouter } from 'react-router-dom';
 import { Home, PlanetDetail } from 'pages';
 
